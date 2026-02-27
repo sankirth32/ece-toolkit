2a12a15cf33c15780d9c770407ec67a7cf82753d
Sankirth32 <bollavaramsankirthreddy06@gmail.com>
2026-02-28 01:21:57 +0530

    Updated App routing

--- ece-toolkit/src/App.jsx
+++ ece-toolkit/src/App.jsx
@@ -1,7 +1,6 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Tools from "./pages/Tools";
-import About from "./pages/About";
 import OhmsPage from "./pages/OhmsPage";
 import RCPage from "./pages/RCPage";
 import BinaryPage from "./pages/BinaryPage";
@@ -9,17 +8,16 @@ import VoltageDividerPage from "./pages/VoltageDividerPage";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/tools" element={<Tools />} />
-        <Route path="/about" element={<About />} />
         <Route path="/tools/ohms" element={<OhmsPage />} />
         <Route path="/tools/rc" element={<RCPage />} />
         <Route path="/tools/binary" element={<BinaryPage />} />
         <Route path="/tools/voltage-divider" element={<VoltageDividerPage />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
